4dbd47e3b81ebefd5d818a3b12525b155fa6de92
NatashaMurashev <nmurashev@gmail.com>
2012-07-23 14:58:55 -0700

    added sign in and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
   has_secure_password
   
-  validates :name, presence: true, length: { maximum: 50 }
+  before_save :create_remember_token
+  
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Hnclone::Application.routes.draw do
   root to: "users#new"
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
-  match '/signup', to: "users#new"
+  
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 end

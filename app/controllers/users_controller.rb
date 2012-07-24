394b184b86b9f0be0dd344c18682e644b31310f2
NatashaMurashev <nmurashev@gmail.com>
2012-07-24 14:35:59 -0700

    updated link view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to HNclone!"
-      redirect_to @user
+      redirect_to root_path
     else
       render "new"
     end

2f495935ab7fa7e6c7723a0a87d2974cca8c3a99
NatashaMurashev <nmurashev@gmail.com>
2012-07-24 14:21:52 -0700

    a user can't upvote a link she submitted

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,14 +5,18 @@ class VotesController < ApplicationController
   # POST /votes.json
   def create
     link = Link.find(params[:link_id])
-    @vote = link.votes.build
-    @vote.value = params[:value]
-    @vote.user = current_user
+    if link.user_id != current_user.id
+      @vote = link.votes.build
+      @vote.value = params[:value]
+      @vote.user = current_user
     
-    if @vote.save
-      flash[:success] = "Thanks for the upvote!"
+      if @vote.save
+        flash[:success] = "Thanks for the vote!"
+      else
+        flash[:error] = "Sorry, your vote did not count"
+      end
     else
-      flash[:error] = "Sorry, your upvote did not count"
+      flash[:error] = "You cannot upvote your own link!"
     end
     
     redirect_to root_path

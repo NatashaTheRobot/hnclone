1bc475c91f4b0faf37f33f5c50ed2080bd01db63
NatashaMurashev <nmurashev@gmail.com>
2012-07-25 21:15:26 -0700

    added url http validation, updated edit link view

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,5 +5,13 @@ class Link < ActiveRecord::Base
   has_many :comments
   has_many :votes
   
+  before_save :format_url
+  
   validates_uniqueness_of :url
+  validates_presence_of :title
+  
+  private
+    def format_url
+      self.url = 'http://' << self.url unless self.url.include?('http://') or self.url.include?('https://')
+    end
 end
